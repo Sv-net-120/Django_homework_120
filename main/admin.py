135f2eb8d1b63a148f0b33c6fe047a3dc462217a
Sv net <Sveta19930323@gmail.com>
2025-04-06 11:33:49 +0300

    Initial commit

---
+++ main/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from main.models import Client, Car, Sale
+
+admin.site.register(Client)
+admin.site.register(Car)
+admin.site.register(Sale)
+
+# зарегистрируйте необходимые модели
---
+++ main/models.py
@@ -0,0 +1,74 @@
+from contextlib import nullcontext
+from tkinter.constants import CASCADE
+
+from django.db import models
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50)
+    date_of_birth = models.DateField()
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.name} {self.middle_name} {self.last_name} {self.phone_number}'
+
+
+GEARBOX_CHOICES = (
+    ('manual', 'Механика'),
+    ('automatic', 'Автомат'),
+    ('вариатор', 'CVT'),
+    ('robot', 'Робот')
+)
+
+FUEL_TYPE_CHOICES = (
+    ('gasoline', 'Бензин'),
+    ('diesel', 'Дизель'),
+    ('hybrid', 'Гибрид'),
+    ('electro', 'Электро')
+)
+
+BODY_TYPE_CHOICES = (
+    ('sedan', 'Седан'),
+    ('hatchback', 'Хэтчбек'),
+    ('SUV', 'Внедорожник'),
+    ('wagon', 'Универсал'),
+    ('minivan', 'Минивэн'),
+    ('pickup', 'Пикап'),
+    ('coupe', 'Купе'),
+    ('cabrio', 'Кабриолет')
+)
+
+
+DRIVE_UNIT_CHOICES = (
+    ('rear', 'Задний'),
+    ('front', 'Передний'),
+    ('full', 'Полный')
+)
+
+class Car(models.Model):
+    id = models.IntegerField(primary_key=True)
+    model = models.CharField(max_length=40)
+    year = models.IntegerField()
+    color = models.CharField(max_length=20)
+    mileage = models.IntegerField()
+    volume = models.DecimalField(max_digits=2, decimal_places=1)
+    body_type = models.CharField(max_length=20, choices=BODY_TYPE_CHOICES)
+    drive_unit = models.CharField(max_length=20, choices=DRIVE_UNIT_CHOICES)
+    gearbox = models.CharField(max_length=20, choices=GEARBOX_CHOICES)
+    fuel_type = models.CharField(max_length=20, choices=FUEL_TYPE_CHOICES)
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='main/images', blank=True, null=True)
+
+    def __str__(self):
+        return f' {self.id}: {self.model}: {self.year}'
+
+class Sale(models.Model):
+    id = models.IntegerField(primary_key=True)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='client')
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='car')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.id} : {self.created_at}'
